94ac4c9070f8b6e70d20550d3caf9ae543b41a65
maxmax <maxkv333@gmail.com>
2019-12-18 14:36:46 +0200

    Web Push Notification BE deploy fix

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,7 +1,7 @@
 require('dotenv').config({ path: '.env' });
 
 module.exports = {
-    mongoURI: process.env.MONGO_URI,
+    mongoURI: process.env.MONGODB_URI,
     privateKey: process.env.PRIVATE_VAPID_KEY,
     publicKey: process.env.PUBLIC_VAPID_KEY
 }
