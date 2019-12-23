4b53e8bd788486a615fae077ea55acbed1a54ca1
maxmax <maxkv333@gmail.com>
2019-12-23 14:31:07 +0200

    Updates

---
+++ precache-manifest.2a4bcac78edfaa1a96ebe280673087c2.js
@@ -0,0 +1,17 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2655ac0d553f3df414d8",
+    "url": "css/main.css"
+  },
+  {
+    "revision": "324be0c01d22485c30b25639518d6e82",
+    "url": "index.html"
+  },
+  {
+    "revision": "2655ac0d553f3df414d8",
+    "url": "js/main.js"
+  },
+  {
+    "url": "js/runtime~main.2b792f343629287fbbb6.js"
+  }
+]);
